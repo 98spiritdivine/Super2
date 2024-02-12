4f51d9c3ccf996aadf61e44fb8012a25088d1d22
98spiritdivine <60680118+98spiritdivine@users.noreply.github.com>
2024-02-12 08:02:39 -0500

    Update Score.js

--- Score.js
+++ Score.js
@@ -57,3 +57,26 @@ function reset() {
     p2Button.disabled = false;
 
 }
+
+const delayedColorChange = (color, delay) => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            document.body.style.backgroundColor = color;
+            resolve();
+        }, delay)
+    })
+}
+
+
+delayedColorChange('red', 1000)
+    .then(() => delayedColorChange('orange', 1000))
+    .then(() => delayedColorChange('yellow', 1000))
+    .then(() => delayedColorChange('green', 1000))
+    .then(() => delayedColorChange('blue', 1000))
+    .then(() => delayedColorChange('indigo', 1000))
+    .then(() => delayedColorChange('violet', 1000))
+     .then(() => delayedColorChange('verdigris', 1000))
+.then(() => delayedColorChange('#8BA6A9', 1000))
+.then(() => delayedColorChange('#3C6BEC', 1000))
+
+
