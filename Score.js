ff7c53972d6aa0d10a98718a85b45d55eb65f49c
98spiritdivine <60680118+98spiritdivine@users.noreply.github.com>
2024-10-13 23:07:52 -0400

    Update Score.js

--- Score.js
+++ Score.js
@@ -69,12 +69,10 @@ const delayedColorChange = (color, delay) => {
 
 
 delayedColorChange('#06d6a0', 5000)
-    .then(() => delayedColorChange('orange', 5000))
-    .then(() => delayedColorChange('yellow', 5000))
-    .then(() => delayedColorChange('green', 5000))
-    .then(() => delayedColorChange('blue', 5000))
+   
+    .then(() => delayedColorChange('violet, 5000))
     .then(() => delayedColorChange('indigo', 50000))
-    .then(() => delayedColorChange('violet', 50000))
+    .then(() => delayedColorChange('grey', 50000))
      .then(() => delayedColorChange('verdigris', 50000))
 .then(() => delayedColorChange('#8BA6A9', 50000))
 .then(() => delayedColorChange('#3C6BEC', 50000))
